ba91f83b68c835ac45d0ef5dd93ccb2e29a8b1e9
Anand Kumar <kumarabhishek31853@gmail.com>
2021-09-05 00:42:25 +0530

    navbar opening issue fix and teams images resized

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,53 +2,58 @@ import { BlueButton } from '../Buttons/Buttons';
 import { NavItem } from './NavItem';
 import Logo from '../../assets/images/hllogo.png';
 import { AiOutlineAlignRight } from 'react-icons/ai';
-import { useEffect } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { join } from '../Data/socials';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({ navControl }) => {
-  useEffect(() => {
-    document.addEventListener('scroll', () => {
-      const navSelect = document.getElementById('navbar');
-      const height = navSelect.offsetHeight;
+const Navbar = ({ navControl }, ref) => {
+	useEffect(() => {
+		document.addEventListener('scroll', () => {
+			const navSelect = document.getElementById('navbar');
+			const height = navSelect.offsetHeight;
 
-      const scroll = window.scrollY;
+			const scroll = window.scrollY;
 
-      if (scroll > height) {
-        navSelect.classList.remove('navbar-transparent');
-      } else {
-        navSelect.classList.add('navbar-transparent');
-      }
-    });
+			if (scroll > height) {
+				navSelect.classList.remove('navbar-transparent');
+			} else {
+				navSelect.classList.add('navbar-transparent');
+			}
+		});
 
-    return () => document.removeEventListener('scroll', null);
-  }, []);
-  return (
-    <nav
-      id="navbar"
-      className="navbar fixed-top navbar-expand-lg navbar-transparent"
-    >
-      <div className="container">
-        <div className="navabr__translate">
-          <a className="navbar__brand" href="/">
-            <img height="30px" src={Logo} alt="DSC BIT" />
-          </a>
-        </div>
-        <nav className="collapse navbar__collapse">
-          <ul className="navbar__nav ml-auto">
-            <NavItem to="/">Home</NavItem>
-            <NavItem to="/events">Events</NavItem>
-            <NavItem to="/teams">Teams</NavItem>
-            <BlueButton style={{ margin: '0 0 0 14px' }} link={join}>
-              Join Us
-            </BlueButton>
-          </ul>
-        </nav>
-        <div className="navbar__collapsible-btn ml-auto" onClick={navControl}>
-          <AiOutlineAlignRight size={24} />
-        </div>
-      </div>
-    </nav>
-  );
+		return () => document.removeEventListener('scroll', null);
+	}, []);
+	return (
+		<nav
+			id='navbar'
+			className='navbar fixed-top navbar-expand-lg navbar-transparent'
+		>
+			<div className='container'>
+				<div className='navabr__translate'>
+					<Link className='navbar__brand' to='/'>
+						<img height='30px' src={Logo} alt='DSC BIT' />
+					</Link>
+				</div>
+				<nav className='collapse navbar__collapse'>
+					<ul className='navbar__nav ml-auto'>
+						<NavItem to='/'>Home</NavItem>
+						<NavItem to='/events'>Events</NavItem>
+						<NavItem to='/teams'>Teams</NavItem>
+						<BlueButton style={{ margin: '0 0 0 14px' }} link={join}>
+							Join Us
+						</BlueButton>
+					</ul>
+				</nav>
+				<div
+					className='navbar__collapsible-btn ml-auto'
+					onClick={navControl}
+					ref={ref}
+				>
+					<AiOutlineAlignRight size={24} />
+				</div>
+			</div>
+		</nav>
+	);
 };
 
-export default Navbar;
+export default forwardRef(Navbar);
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,40 +2,62 @@ import { AiFillHome } from 'react-icons/ai';
 import Image from '../../assets/images/Logo-with-name.png';
 import { NavItem } from '../Navbar/NavItem';
 import { BsFillPeopleFill, BsFillInfoSquareFill } from 'react-icons/bs';
+import { useEffect, useRef } from 'react';
 
-const Sidebar = ({ open, setClose }) => {
-  return (
-    <section className={`sidebar ${open ? 'sidebar-show' : ''} `}>
-      <div className={`sidebar__container ${open ? 'show-content' : ''}`}>
-        <div className="image">
-          <img height="120px" src={Image} alt="GDSC BITS" />
-        </div>
-        <div className="sidebar__content">
-          <ul className="sidebar__links">
-            <NavItem onClick={setClose} to="/">
-              <AiFillHome style={{ marginRight: '0.5rem' }} />
-              <p>Home</p>
-            </NavItem>
-            <NavItem onClick={setClose} to="/events">
-              <BsFillInfoSquareFill style={{ marginRight: '0.5rem' }} />
-              Events
-            </NavItem>
-            <NavItem onClick={setClose} to="/teams">
-              <BsFillPeopleFill style={{ marginRight: '0.5rem' }} />
-              Teams
-            </NavItem>
-          </ul>
-        </div>
-        <footer className="fixed-bottom-container text-center w-100">
-          <p className="faded-text">
-            MIT License
-            <br />
-            By GDSC BIT, Sindri
-          </p>
-        </footer>
-      </div>
-    </section>
-  );
+const Sidebar = ({ open, setClose, navRef }) => {
+	const sidebarRef = useRef(null);
+	useEffect(() => {
+		const handleBodyClick = (e) => {
+			if (
+				sidebarRef.current.contains(e.target) ||
+				navRef.current.contains(e.target)
+			)
+				return;
+			setClose();
+		};
+
+		document.body.addEventListener('click', handleBodyClick);
+
+		return () => {
+			document.body.removeEventListener('click', handleBodyClick);
+		};
+	}, [setClose, navRef]);
+
+	return (
+		<section
+			className={`sidebar ${open ? 'sidebar-show' : ''} `}
+			ref={sidebarRef}
+		>
+			<div className={`sidebar__container ${open ? 'show-content' : ''}`}>
+				<div className='image'>
+					<img height='120px' src={Image} alt='GDSC BITS' />
+				</div>
+				<div className='sidebar__content'>
+					<ul className='sidebar__links'>
+						<NavItem onClick={setClose} to='/'>
+							<AiFillHome style={{ marginRight: '0.5rem' }} />
+							<p>Home</p>
+						</NavItem>
+						<NavItem onClick={setClose} to='/events'>
+							<BsFillInfoSquareFill style={{ marginRight: '0.5rem' }} />
+							Events
+						</NavItem>
+						<NavItem onClick={setClose} to='/teams'>
+							<BsFillPeopleFill style={{ marginRight: '0.5rem' }} />
+							Teams
+						</NavItem>
+					</ul>
+				</div>
+				<footer className='fixed-bottom-container text-center w-100'>
+					<p className='faded-text'>
+						MIT License
+						<br />
+						By GDSC BIT, Sindri
+					</p>
+				</footer>
+			</div>
+		</section>
+	);
 };
 
 export default Sidebar;
